ef54a325ae193a2df9f04ae87f1f1b2161eddbd8
khalid105 <khalid105@yahoo.com>
2025-05-06 21:41:06 -0400

    Adding new file

---
+++ MavenProject/src/test/java/com/test/TestNgAssignment1.java
@@ -0,0 +1,27 @@
+package com.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestNgAssignment1 {
+	WebDriver driver;
+
+	@Test
+	public void LaunchBrowser()
+	{
+		driver = new ChromeDriver();
+
+		driver.get("https://testautomationpractice.blogspot.com/");
+
+		driver.manage().window().maximize();
+
+		System.out.println(driver.getTitle());
+		
+		driver.quit();
+	}
+
+	
+
+
+}
